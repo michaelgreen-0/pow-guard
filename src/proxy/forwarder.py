3fde7629508ce645831854568b3a1c240609b0e7
user <m_green@hotmail.co.nz>
2025-05-06 21:48:06 +1000

    chore: cleanup forwarder

--- src/proxy/forwarder.py
+++ src/proxy/forwarder.py
@@ -4,14 +4,10 @@ from yarl import URL
 from env import BACKEND_URL
 
 async def forward_request(request: Request):
-    # Build the complete target URL
     url = str(URL(BACKEND_URL).with_path(request.url.path).with_query(request.url.query))
-
-    # Clean headers: use dict directly and remove Host
     headers = dict(request.headers)
     headers.pop("host", None)
 
-    # Forward the request
     async with aiohttp.ClientSession() as session:
         async with session.request(
             method=request.method,
