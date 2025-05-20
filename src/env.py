3bd8a07a1ecad4c04753f40441f66c63bf688b79
Michael Green <m_green@hotmail.co.nz>
2025-05-20 13:38:07 +0000

    testing

--- src/env.py
+++ src/env.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
+from os import environ
 
 load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
 BACKEND_URL = os.getenv("BACKEND_URL")
-POW_DIFFICULTY = int(os.getenv("POW_DIFFICULTY"))
+# POW_DIFFICULTY = int(os.getenv("POW_DIFFICULTY"))
+POW_DIFFICULTY = int(environ["POW_DIFFICULTY"])
