2d79cd0621a044e96ebb32fb794716ecba2a0913
Michael Green <m_green@hotmail.co.nz>
2025-05-24 02:09:30 +0000

    chore: update middleware docstring

--- src/middlewares/verifier.py
+++ src/middlewares/verifier.py
@@ -7,7 +7,7 @@ from ..utils.redis import get_redis
 
 
 async def verify_pow_middleware(request: Request, call_next):
-    """Middleware for routing a request as it arrives
+    """Middleware for deciding where to route a request as it arrives.
     - If the request is already going for verification (starts with /pow) then continue to pow service
     - we're also not interested in requiring pow verification when serving static files
     - If the request is to another resource then:
